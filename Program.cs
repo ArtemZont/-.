3aa43c7b99b9b86e1efc22bd1613c4768e3e1351
Артём <124985495+ArtemZont@users.noreply.github.com>
2023-05-26 12:12:57 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,46 @@
+using BusinessLogic.Interfaces;
+using BusinessLogic.Services;
+using DataAccess.Wrapper;
+using Microsoft.EntityFrameworkCore;
+using DataAccess.Models;
+
+
+namespace WebAPI1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddDbContext<WebShopContext>(optionsAction: options => options.UseSqlServer(connectionString: "Server=DESKTOP-2LGOQKK;Database=WebShop;Trusted_Connection=true"));
+
+            builder.Services.AddScoped<IRepositoryWrapper, RepositoryWrapper>();
+            builder.Services.AddScoped<IUserService, UserService>();
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
